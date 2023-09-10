6fb9c7344c52e8a24cc742ef7da95d4420569059
Fadhil Muhammad <fadhilmuhammad.fd@gmail.com>
2023-09-10 21:32:59 +0700

    commit

--- my_project/settings.py
+++ my_project/settings.py
@@ -117,7 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    join(BASE_DIR, 'static'),
+)
 
 STATIC_ROOT = join(BASE_DIR, 'staticfiles')
 
