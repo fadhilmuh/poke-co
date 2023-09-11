389e2fc9758addf4aeb0df064dc2274eabf02641
Fadhil Muhammad <fadhilmuhammad.fd@gmail.com>
2023-09-11 14:41:05 +0700

    rename app name

--- main/models.py
+++ main/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-    name = models.CharField(max_length=255)
-    amount = models.IntegerField()
-    description = models.TextField()
+    name = models.CharField(max_length=255, name="name")
+    amount = models.IntegerField(name="amount")
+    rarity = models.IntegerField(name="rarity", default=0)
+    power = models.FloatField(name="power", default=0)
+    description = models.TextField(name="description")
